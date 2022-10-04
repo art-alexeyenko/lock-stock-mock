75df38330fac2c3e849c09802f696be60550d957
Artem Alexeyenko <ala@sitecore.net>
2022-10-04 18:39:42 -0400

    init

---
+++ mock-express/index.js
@@ -0,0 +1,21 @@
+// Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+app.get("/api/graphql/v1", (req, res) => {
+    res.json({ site: { siteInfo: { redirects: {} }}});
+})
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+module.exports = app;
