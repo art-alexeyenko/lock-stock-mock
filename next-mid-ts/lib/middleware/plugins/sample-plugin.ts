19972844b78f456b314d910353e3f030ac9a7e67
Artem Alexeyenko <ala@sitecore.net>
2022-10-09 20:15:45 -0400

    no message

--- next-mid-ts/lib/middleware/plugins/sample-plugin.ts
+++ next-mid-ts/lib/middleware/plugins/sample-plugin.ts
@@ -1,10 +1,40 @@
 import { NextRequest, NextResponse } from 'next/server';
 import {debug} from 'debug';
 
+type RedirectsQueryResult = {
+    site: { siteInfo: { redirects: RedirectInfo[] } | null };
+  };
+
+  export type RedirectInfo = {
+    pattern: string;
+    target: string;
+    redirectType: string;
+    isQueryStringPreserved: boolean;
+    locale: string;
+  };
+
 class SamplePlugin  {
 
   constructor(private endpoint: string) {
   }
+
+  query = /* GraphQL */ `
+  query RedirectsQuery($siteName: String!) {
+    site {
+      siteInfo(site: $siteName) {
+        redirects {
+          pattern
+          target
+          redirectType
+          isQueryStringPreserved
+          locale
+        }
+      }
+    }
+  }
+`;
+
+  timeout = 1000;
 
   /**
    * exec async method - to find coincidence in url.pathname and redirects of site
@@ -14,26 +44,40 @@ class SamplePlugin  {
   async exec(): Promise<NextResponse> {
 
     const abort = new AbortController();
+    let abortTimeout: NodeJS.Timeout;
+
+      if (this.timeout) {
+        abortTimeout = setTimeout(() => {
+          abort.abort();
+        }, this.timeout);
+      }
+
+      const variables = {siteName: '123'};
 
     let res = await new Promise((resolve, reject) => fetch(this.endpoint, {
-        method: 'POST',
+        method: "POST",
         signal: abort.signal,
         headers: {
           'Content-Type': 'application/json',
+          // ...this.headers,
         },
+        body: JSON.stringify({
+          query: this.query,
+          variables: variables,
+        }),
       })
       .then(res => res.json())
-      .then((data: any) => {
-        debug('mock:')('response: %o', data)
+      .then((data: RedirectsQueryResult) => {
+        clearTimeout(abortTimeout);
+        debug('mock')('response: %o', data);
         resolve(data);
       })
-      .catch((error) => {
-        debug('mock:')('response error: %o', error.response || error.message || error);
+      .catch((error: any) => {
+        debug('mock')('response error: %o', error.response || error.message || error);
         reject(error);
-      })
-    );
+      }));
     return NextResponse.next();
   }
 }
 
-export const samplePlugin = new SamplePlugin(process.env.FETCH_ENDPOINT || 'http://localhost:5000/api');
+export const samplePlugin = new SamplePlugin(process.env.FETCH_ENDPOINT || "https://lock-stock-mock.vercel.app/api");
